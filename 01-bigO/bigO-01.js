ab5719abad6c482e0ad9503d0608e3b7c3ec4fca
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-15 15:35:39 -0600

    another example of O(n)

--- 01-bigO/bigO-01.js
+++ 01-bigO/bigO-01.js
@@ -35,8 +35,30 @@ console.log(`Time Elapsed Solution2: ${(timeF2 - timeS2) / 1000} seconds`);
     Aquí no tenemos un número definido, puesto que la mayoría de las operaciones dependen del
     número que asignemos a "n" (ej. el <= se ejecuta las veces que se ejecute el for, mismo caso
     con el i++, etc.).
+    BigO: O(n)
 
     Forma 2: 
     No. de operaciones: 3 (*, +, /)
-    No importa el valor de "n", siempre son 3 operaciones la que se realizan
+    No importa el valor de "n", siempre son 3 operaciones la que se realizan.
+    BigO: O(1)
+*/
+
+// -------------------- Más ejemplos de BigO --------------------
+function countUpAndDown(n) {
+    console.log('Going up!');
+    for(let i = 0; i < n; i++) {
+        console.log(i);
+    }
+    console.log('At the top! \nGoing down...');
+    for(let j = n - 1; j >= 0; j--) {
+        console.log(j);
+    }
+    console.log('Back down. Bye!')
+}
+countUpAndDown(10);
+
+/* 
+    En el ejemplo anterior, vemos que las operaciones son 1 por cada console.log y en cada ciclo
+    es O(n), por lo que podemos decir que es: 2 O(n) + 3.
+    Como debemos enfocarnos en el 'big picture', esto es igual a O(n).
 */
