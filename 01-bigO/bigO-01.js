69b2974e72b8675aaae7ed32c519132c6304152b
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-15 15:41:49 -0600

    example of O(n^2)

--- 01-bigO/bigO-01.js
+++ 01-bigO/bigO-01.js
@@ -62,3 +62,18 @@ countUpAndDown(10);
     es O(n), por lo que podemos decir que es: 2 O(n) + 3.
     Como debemos enfocarnos en el 'big picture', esto es igual a O(n).
 */
+
+function printAllPairs(n) {
+    for(let i = 0; i < n; i++){
+        for(let j = 0; j < n; j++) {
+            console.log(i, j);
+        }
+    }
+}
+printAllPairs(4);
+
+/* 
+    En el ejemplo anterior, vemos que tenemos un ciclo (O(n)), pero dentro tenemos otro ciclo, 
+    por lo que por cada operación del ciclo de afuera se ejecuta otra vez O(n), esto no es 
+    2 veces O(n), sino n * n, es decir O(n^2).
+*/
