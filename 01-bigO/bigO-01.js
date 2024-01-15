4da95b417e0a8548bb984bbc9cd714c7aa5ee34b
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-15 15:13:42 -0600

    notes about counting operations

--- 01-bigO/bigO-01.js
+++ 01-bigO/bigO-01.js
@@ -1,7 +1,8 @@
-// Problema: Escribir una función que calcule la suma de todos los números de 1 a n (incluyendo n);
+// Problema: 
+// Escribir una función que calcule la suma de todos los números de 1 a n (incluyendo n);
 // Ej: si tenemos 6 => 1+2+3+4+5+6 = 21
 
-// Forma 1
+// -------------------- Forma 1 --------------------
 function addUpTo(n) {
     let total = 0;
     for(let i = 1; i <= n; i++) {
@@ -13,9 +14,8 @@ let timeS1 = performance.now();
 addUpTo(1000000000);
 let timeF1 = performance.now();
 console.log(`Time Elapsed Solution1: ${(timeF1 - timeS1) / 1000} seconds`);
-// console.log(addUpTo(6));
 
-// Forma 2
+// -------------------- Forma 2--------------------
 function addUpTo2(n) {
     return n * (n + 1) / 2;
 }
@@ -23,4 +23,17 @@ let timeS2 = performance.now();
 addUpTo(1000000000);
 let timeF2 = performance.now();
 console.log(`Time Elapsed Solution2: ${(timeF2 - timeS2) / 1000} seconds`);
-// console.log(addUpTo2(6));
+
+/* 
+    NOTA: A pesar de que la Forma 2 es más rápida que la 1, contar el tiempo no siempre es una forma
+    confiable de medir el performance de un código, puesto que depende de la máquina donde se ejecute.
+    Contar el número de operaciones que un código realiza SÍ es buena forma de comparar cuando uno
+    es mejor que otro.
+
+    Forma 1:
+    No. de operaciones: 
+
+    Forma 2: 
+    No. de operaciones: 3 (*, +, /)
+    No importa el valor de "n", siempre son 3 operaciones la que se realizan
+*/
