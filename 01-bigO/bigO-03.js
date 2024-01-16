3e6036b3dfbc2fc1c45a487b069646bf72ace909
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-15 20:33:23 -0600

    space complexity O(n)

--- 01-bigO/bigO-03.js
+++ 01-bigO/bigO-03.js
@@ -44,3 +44,19 @@ function logAtMost10(n) {
 /* 
     Space complexity: O(1) space.
 */
+
+
+function onlyElementsAtEvenIndex(array) {
+    var newArray = Array(Math.ceil(array.length / 2));
+    for (var i = 0; i < array.length; i++) {
+        if (i % 2 === 0) {
+            newArray[i / 2] = array[i];
+        }
+    }
+    return newArray;
+}
+
+/* 
+    Space complexity: O(n) space.
+    Ya que el tamaño del nuevo array depende del input.
+*/
