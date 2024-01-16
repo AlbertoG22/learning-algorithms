f20ebeea220a21c5d6fc94891f9856ffcbaa7123
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-16 13:24:33 -0600

    psa step 1

--- 02-problem-solving-approach/psa.js
+++ 02-problem-solving-approach/psa.js
@@ -8,3 +8,20 @@
         - Paso 4: Solve Or Simplify
         - Paso 5: Look Back and Refactor
 */
+
+
+// Ejemplo:
+// ===============================================================================================
+// Problem: Write a function which takes two numbers and return their sum.
+// ===============================================================================================
+
+// ---------------------- PASO 1: ----------------------
+/* 
+    Para entender el problema hay ciertas preguntas que nos podemos hacer (o al entrevistador
+    para comprender lo que se nos pide):
+        1. Can I restate the problem in my own words?
+        2. What are the inputs that go into the problem?
+        3. What are the outputs that should come from the solution to the problem?
+        4. Can the outputs be determined from the inputs? Do I have enough info to solve the problem?
+        5. How should I label the important pieces of data that are a part of the problem?
+*/
