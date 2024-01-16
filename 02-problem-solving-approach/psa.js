4c92f5a31ea8a34699f680daa42da1e623f240cb
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-16 13:23:13 -0600

    problem solving approach 01

---
+++ 02-problem-solving-approach/psa.js
@@ -0,0 +1,10 @@
+/* 
+    Antes de empezar a resolver un problema directamente, debemos primero razonar y entender qué
+    es lo que debemos realizar, entender el problema. Hay ciertos pasos que podemos implementar 
+    y que nos ayudarán a esto:
+        - Paso 1: Understand the Problem
+        - Paso 2: Concrete Examples
+        - Paso 3: Break it down
+        - Paso 4: Solve Or Simplify
+        - Paso 5: Look Back and Refactor
+*/
