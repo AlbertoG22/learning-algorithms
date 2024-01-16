0c328c951627e7d6fac353aa54cedfded4c381d1
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-15 18:29:34 -0600

    more examples of bigO

--- 01-bigO/bigO-02.js
+++ 01-bigO/bigO-02.js
@@ -34,3 +34,37 @@ printAllPairs(4);
     por lo que por cada operación del ciclo de afuera se ejecuta otra vez O(n), esto no es 
     2 veces O(n), sino n * n, es decir O(n^2).
 */
+
+
+// ------------------------------------------------------------------
+function logAtLeast5(n) {
+    for (let i = 1; i <= Math.max(5, n); i++) {
+      console.log(i);
+    }
+}
+
+/* 
+    En el ejemplo anterior se tiene una función donde imprime los números del 1 hasta "n", 
+    siempre y cuando "n" sea mayor a 5, de lo contrario imprime del 1 al 5.
+    
+    BigO: O(n)
+    En este ejemplo el caso que nos interesa para determinar el BigO es el "peor" de lo casos,
+    es decir, a medida que "n" vaya aumentando, más serán las operaciones que realizará la fn.
+*/
+
+
+// ------------------------------------------------------------------
+function logAtMost5(n) {
+    for (var i = 1; i <= Math.min(5, n); i++) {
+      console.log(i);
+    }
+}
+
+/* 
+    En el ejemplo anterior es al revés, la función imprime los números del 1 al 5 siempre y
+    cuando "n" sea mayor a 5, de lo contrario imprime del 1 hasta n.
+    
+    BigO: O(1)
+    En este ejemplo a medida que "n" vaya aumentando, las operaciones se mantienen, por lo que 
+    el máximo es 5.
+*/
