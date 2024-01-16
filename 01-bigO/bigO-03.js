be3c405f82105901202739a393fdbde25c8275b4
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-15 20:30:55 -0600

    space complexity O(n)

--- 01-bigO/bigO-03.js
+++ 01-bigO/bigO-03.js
@@ -1,4 +1,4 @@
-// TIME COMPLEXITY
+//                                      TIME COMPLEXITY
 // BigO también sirve para definir la complejidad en cuanto a espaico en memoria que ocupa el algoritmo
 function sum(arr) {
     let total = 0;
@@ -12,7 +12,24 @@ function sum(arr) {
     Para calcular el espacio que ocupa la función anterior, vemos que tenemos la variable de la
     línea 4, y la variable i del ciclo for.
     No importa lo que se pasa como parámetro, no se va a crear nuevas variable u ocupar más espacio
-    en memoria (solo se actualiza), por lo que la complejidad es:
-    2O(n) --> O(n).
+    en memoria (solo se actualiza). 
+    Por lo que la complejidad es: O(2) --> O(1) space.
     Siempre es el mismo, no importa el tamaño del input.
+*/
+
+
+function double(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      newArr.push(2 * arr[i]);
+    }
+    return newArr;
+}
+
+/* 
+    Para el space complexity del ejemplo anterior tenemos la creación del array (línea 22),
+    y la variable de iteración. El tema aquí es que el tamaño del array va a depender directamente
+    al la longitud del input que le pasemos, es decir, el espacio que va a ocupar es proporcional 
+    a "n" (input).
+    Por lo tanto, la complejidad será: O(n) + 2 --> O(n) space.
 */
