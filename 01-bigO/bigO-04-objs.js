354e6d52b8b0cd4bbbaaaf539479ed42c775277b
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-16 11:59:06 -0600

    bigO of objects notes

---
+++ 01-bigO/bigO-04-objs.js
@@ -0,0 +1,16 @@
+//                           BigO of Objects
+/* 
+    Los objetos son buenos cuando:
+    - no se necesita tener un orden,
+    - se necesita acceder, insertar o remover elementos de forma rápida.
+        * Insertion     - O(1)
+        * Removal       - O(1)
+        * Searching     - O(n)
+        * Access        - O(1)
+    
+    - BigO Object Methods:
+        * Object.keys       - O(n)
+        * Object.values     - O(n)
+        * Object.entries    - O(n)
+        * hasOwnProperty    - O(1)
+*/
