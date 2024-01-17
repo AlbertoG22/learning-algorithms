6ccb72136fc756cc1d72a64fa4f2017209901439
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-17 13:43:50 -0600

    fcp anagram challenge

---
+++ 03-problem-solving-patterns/01-frequency-counter-pattern/fcp-02-anagram.js
@@ -0,0 +1,21 @@
+// ------------------------------ Anagram Challenge ----------------------------------------
+/* 
+    Nota:Un anagrama es una palabra, frase o nombre formado al reorganizar las letras de otra 
+    palabra, por ejemplo la palabra 'cinema', se puede formar de la palabra 'iceman'.
+*/
+
+// ===============================================================================================
+/* 
+    Dado 2 strings, escribir una función que determine si el segundo string es un anagrama del primero.
+    
+    Ejemplo:
+    validAnagram('', '') // true
+    validAnagram('aaz', 'zza') // false
+    validAnagram('anagram', 'nagaram') // true
+    validAnagram("rat","car") // false) // false
+    validAnagram('awesome', 'awesom') // false
+    validAnagram('qwerty', 'qeywrt') // true
+    validAnagram('texttwisttime', 'timetwisttext') // true
+*/
+// ===============================================================================================
+
