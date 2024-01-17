14cdab288d78907caa9239ea1561d55e1e63dd7f
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-17 17:22:48 -0600

    multiple pointers pattern notes

---
+++ 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-01-intro.js
@@ -0,0 +1,8 @@
+// ------------------------------ Multiple Pointers Pattern ----------------------------------------
+/* 
+    La idea de este patrón es crear punteros o valores que correspondan a un índice o posición y se 
+    muevan hacia el principio, el final o el medio en función de una determinada condición.
+    Se trabaja sobre dos estructuras lineales (array/string), y la idea es ir buscando un par de 
+    valores o algo que cumpla una condición. Se usan dos referencias, un índice al inicio del 
+    array/string y el otro al final, y se trabaja el uno hacia el otro.
+*/
