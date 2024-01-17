66d528a57b7acd4a8ba199a038d0be1a7fdd0c31
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-17 12:09:33 -0600

    freq counter pattern notes

---
+++ 03-problem-solving-patterns/01-frequency-counter-pattern/fcp-01.js
@@ -0,0 +1,8 @@
+// ---------------------- Frequency Counter Pattern ----------------------------------------
+/* 
+    La idea de este patrón es usar objetos para almacenar un conjunto de valores y sus 
+    frecuencias. 
+    Esto puede evitarnos usar ciclos anidados u operaciones cuadráticas (O(n2)) con 
+    arrays/strings.
+*/
+
