71da2b16189471a385973a8a023f6d2eda3b4f4d
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-17 17:36:06 -0600

    mpp naive solution

--- 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-01-intro.js
+++ 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-01-intro.js
@@ -20,3 +20,14 @@
     sumZero([1,2,3]) // undefined
 */
 // ===============================================================================================
+
+// -------- Solución compleja: --------
+function sumZero(arr){
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i+1; j < arr.length; j++){
+            if(arr[i] + arr[j] === 0){
+                return [arr[i], arr[j]];
+            }
+        }
+    }
+}
