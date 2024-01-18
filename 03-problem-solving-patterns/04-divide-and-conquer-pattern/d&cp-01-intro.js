6c28b99fa8c4e5e5464ed4101c40955134a231fc
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-18 07:45:30 -0600

    divide and conquer pattern notes

---
+++ 03-problem-solving-patterns/04-divide-and-conquer-pattern/d&cp-01-intro.js
@@ -0,0 +1,7 @@
+// ------------------------------ Divide and Conquer Pattern ----------------------------------------
+/* 
+    Este patrón consiste en tomar un gran conjunto de datos, ya sea un array, string, linked list, 
+    tree, etc. y en lugar de iniciar de izquierda a derecha la búsqueda de un valor, vamos a dividir 
+    los datos en piezas pequeñas, para así hacer algo sobre esta pequeña cantidad de datos, y después 
+    determinar a dónde seguir buscando.
+*/
