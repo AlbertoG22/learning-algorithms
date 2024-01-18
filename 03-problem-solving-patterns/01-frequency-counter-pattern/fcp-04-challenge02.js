bcc6d4406061d46b3e65c21f3aa067c732c2e178
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-18 13:25:37 -0600

    fcp - practice challenge 02 (another solution)

--- 03-problem-solving-patterns/01-frequency-counter-pattern/fcp-04-challenge02.js
+++ 03-problem-solving-patterns/01-frequency-counter-pattern/fcp-04-challenge02.js
@@ -1,4 +1,4 @@
-// ------------------------------ Multiple Pointers Frequency Challenge ----------------------------------------
+// ------------------- Frequency Counter / Multiple Pointers Challenge ------------------------------
 /* 
     Problema:
     Escribe una función llamada 'areThereDuplicates' que acepte un número indefinido de argumentos y 
@@ -19,16 +19,31 @@
 
 // ---------------- Mi solución FCP ----------------
 function areThereDuplicates(...args) {
+    if(args.length === 0) return false;
     const freqCount = {};
     for(const el of args){
-        if(!freqCount[el]) freqCount[el] = 1;
-        else return true;
+        if(freqCount[el]) return true;
+        freqCount[el] = 1;
     }
     return false;
 }
 areThereDuplicates(1, 2, 3); // false
 
-
+// ---------------- Mi solución MPP ----------------
+function areThereDuplicates(...args) {
+    if(args.length === 0) return false;
+    let indexA = args[0];
+    let obj = {};
+    obj[indexA] = 1;
+    
+    for(let i = 1; i < args.length; i++) {
+        let currEl = args[i];
+        if(currEl in obj) return true;
+        obj[currEl] = 1;
+    }
+    return false;
+}
+areThereDuplicates('a', 'b', 'c', 'a'); // true 
 
 
 
