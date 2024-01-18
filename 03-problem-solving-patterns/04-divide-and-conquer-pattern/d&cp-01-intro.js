701eef4d3b5d9f42bbdd706b40bb8e8242bff988
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-18 07:45:54 -0600

    d&cp problem description

--- 03-problem-solving-patterns/04-divide-and-conquer-pattern/d&cp-01-intro.js
+++ 03-problem-solving-patterns/04-divide-and-conquer-pattern/d&cp-01-intro.js
@@ -5,3 +5,17 @@
     los datos en piezas pequeñas, para así hacer algo sobre esta pequeña cantidad de datos, y después 
     determinar a dónde seguir buscando.
 */
+
+// ===============================================================================================
+/* 
+    Problema:
+    Escriba una función llamada 'search' que acepte un array de enteros ORDENADOS y un número n.
+    La función deberá retornar el índice donde se encuentra ese valor n en el array. Si no encuentra, 
+    devuelve -1.
+
+    Output:
+    search([1,2,3,4,5,6],4) // 3
+    search([1,2,3,4,5,6],6) // 5
+    search([1,2,3,4,5,6],11) // -1
+*/
+// ===============================================================================================
