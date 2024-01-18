def9ccb715b6b8aba826f6896c650dc42be6f8bf
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-18 11:59:07 -0600

    fcp - practice challenge 02 (problem description)

---
+++ 03-problem-solving-patterns/01-frequency-counter-pattern/fcp-04-challenge02.js
@@ -0,0 +1,46 @@
+// ------------------------------ Multiple Pointers Frequency Challenge ----------------------------------------
+/* 
+    Problema:
+    Escribe una función llamada 'areThereDuplicates' que acepte un número indefinido de argumentos y 
+    verifique si hay duplicados entre los argumentos pasados. 
+    Nota: Puede resolver esto usando el FCP o el MPP.
+    - Solución debe ser: 
+        * Time - O(n).
+        * Space - O(n)
+    - Bonus:
+        * Time - O(n log n)
+        * Space - O(1)
+
+    Output:
+    areThereDuplicates(1, 2, 3); // false
+    areThereDuplicates(1, 2, 2); // true 
+    areThereDuplicates('a', 'b', 'c', 'a'); // true 
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
