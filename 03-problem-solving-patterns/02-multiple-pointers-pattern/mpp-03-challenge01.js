a11d7bf99258873ec1aec69f95850dcaea293c51
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-20 12:52:50 -0600

    mpp - practice challenge 01 (course solution)

--- 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-03-challenge01.js
+++ 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-03-challenge01.js
@@ -28,3 +28,17 @@ function averagePair(array, average) {
     return false;
 }
 averagePair([1,3,3,5,6,7,10,12,19],8); // true
+
+
+// ---------------- Solución del curso ----------------
+function averagePair(arr, num){
+    let start = 0
+    let end = arr.length-1;
+    while(start < end){
+      let avg = (arr[start]+arr[end]) / 2 
+      if(avg === num) return true;
+      else if(avg < num) start++
+      else end--
+    }
+    return false;
+}
