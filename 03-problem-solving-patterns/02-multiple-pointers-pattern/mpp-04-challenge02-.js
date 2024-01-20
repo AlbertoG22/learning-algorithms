d6719b9428b1830b7c061677aac6370fb87c99a1
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-20 13:13:13 -0600

    mpp - practice challenge 02 (another course solution recursive)

--- 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-04-challenge02-.js
+++ 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-04-challenge02-.js
@@ -53,3 +53,11 @@ function isSubsequence(str1, str2) {
     }
     return false;
 }
+
+// -------------- Otra solución del curso (Recursive but not O(1) space) ----------------
+function isSubsequence(str1, str2) {
+    if(str1.length === 0) return true;
+    if(str2.length === 0) return false;
+    if(str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
+    return isSubsequence(str1, str2.slice(1));
+}
