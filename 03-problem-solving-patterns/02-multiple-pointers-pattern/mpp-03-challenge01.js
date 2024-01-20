207aaa377d06e0644e1587d8b95781ce35d05e46
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-19 23:02:26 -0600

    mpp - practice challenge 01 (my solution)

--- 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-03-challenge01.js
+++ 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-03-challenge01.js
@@ -14,3 +14,17 @@
     averagePair([-1,0,3,4,5,6], 4.1); // false
     averagePair([],4); // false
 */
+
+function averagePair(array, average) {
+    let startIndex = 0;
+    let endIndex = array.length - 1;
+
+    while(startIndex < endIndex){
+        let actualAverage = (array[startIndex] + array[endIndex]) / 2;
+        if(actualAverage === average) return true;
+        else if (actualAverage > average) endIndex--;
+        else startIndex++;
+    }
+    return false;
+}
+averagePair([1,3,3,5,6,7,10,12,19],8); // true
