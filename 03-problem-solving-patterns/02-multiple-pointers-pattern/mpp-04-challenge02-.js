67942d3d31a95f9cee88629fac992b7f9a827f33
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-20 00:29:56 -0600

    mpp - practice challenge 02 (another solution) small changes

--- 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-04-challenge02-.js
+++ 03-problem-solving-patterns/02-multiple-pointers-pattern/mpp-04-challenge02-.js
@@ -30,12 +30,10 @@ isSubsequence('abc', 'dabracadabra'); // true
 
 // ---------------- Otra solución hecha por mí ----------------
 function isSubsequence(str1, str2) {
-    let arr1 = str1.split('');
-    let arr2 = str2.split('');
     let i = 0;
 
-    for(let j = 0; j < arr2.length; j++) {
-        if(arr1[i] === arr2[j]) {
+    for(let j = 0; j < str2.length; j++) {
+        if(str1[i] === str2[j]) {
             i++;
         }
     }
