7bfefeb54423eb1ee19b9a989c0b5a2a3bb06faa
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-24 12:31:56 -0600

    swp - practice challenge 03 (problem description)

---
+++ 03-problem-solving-patterns/03-sliding-window-pattern/swp-04-challenge03.js
@@ -0,0 +1,17 @@
+// ---------------------------- Find Longest Substring Challenge ----------------------------------------
+/* 
+    Problema:   
+    Escriba una función llamada 'findLongestSubstring', que acepte un string y devuelva la longitud 
+    del substring más largo con todos los caracteres distintos.
+    - Solución debe ser: 
+        * Time - O(n).
+
+    Output:
+    findLongestSubstring('') // 0
+    findLongestSubstring('rithmschool') // 7
+    findLongestSubstring('thisisawesome') // 6
+    findLongestSubstring('thecatinthehat') // 7
+    findLongestSubstring('bbbbbb') // 1
+    findLongestSubstring('longestsubstring') // 8
+    findLongestSubstring('thisishowwedoit') // 6
+*/
