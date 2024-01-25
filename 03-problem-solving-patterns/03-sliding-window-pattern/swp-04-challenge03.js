657674e8f102846e4142739d09ed701228fa3e5d
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-24 21:50:51 -0600

    swp - practice challenge 03 (my solution)

--- 03-problem-solving-patterns/03-sliding-window-pattern/swp-04-challenge03.js
+++ 03-problem-solving-patterns/03-sliding-window-pattern/swp-04-challenge03.js
@@ -7,11 +7,37 @@
         * Time - O(n).
 
     Output:
-    findLongestSubstring('') // 0
-    findLongestSubstring('rithmschool') // 7
-    findLongestSubstring('thisisawesome') // 6
-    findLongestSubstring('thecatinthehat') // 7
-    findLongestSubstring('bbbbbb') // 1
-    findLongestSubstring('longestsubstring') // 8
-    findLongestSubstring('thisishowwedoit') // 6
+    findLongestSubstring(''); // 0
+    findLongestSubstring('rithmschool'); // 7
+    findLongestSubstring('bbbbbb'); // 1
+    findLongestSubstring('thisishowwedoit'); // 6
+
+    findLongestSubstring('thisisawesome'); // 6  --> yo digo que es 5
+    findLongestSubstring('thecatinthehat'); // 7  --> yo digo que es 5
+    findLongestSubstring('longestsubstring'); // 8  --> yo digo que es 7
 */
+
+function findLongestSubstring(str){
+    if(str.length === 0) return 0;
+
+    let tempStr = `${str[0]}`;
+    let maxSubstr = 1;
+    let tempSubstr = 1;
+
+    for(let i = 1; i < str.length; i++) {
+        let letter = str[i];
+
+        if(!(tempStr.includes(letter))) {
+            tempStr += str[i];
+            tempSubstr++;
+        }
+        else {
+            tempStr = letter;
+            tempSubstr = 1;
+        }
+        if(tempSubstr > maxSubstr) maxSubstr = tempSubstr;
+    }
+    return maxSubstr;
+}
+findLongestSubstring('thisisawesome'); // 6
+findLongestSubstring('rithmschool'); // 7
