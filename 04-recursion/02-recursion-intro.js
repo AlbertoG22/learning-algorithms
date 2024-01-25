2e4cadae72b2181235e9c141c561cc635661074b
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-25 17:42:21 -0600

    recursion example

--- 04-recursion/02-recursion-intro.js
+++ 04-recursion/02-recursion-intro.js
@@ -7,3 +7,15 @@
         * Input diferente.
     
 */
+
+// Ejemplo:
+function countDown(num){
+    if(num <= 0) {
+        console.log("All done!");
+        return;
+    }
+    console.log(num);
+    num--;
+    countDown(num);
+}
+countDown(5);
