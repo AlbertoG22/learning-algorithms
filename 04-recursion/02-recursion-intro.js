08ff793fcc50de50b168599076af0e74ac549291
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-25 17:40:37 -0600

    recursion explanation

---
+++ 04-recursion/02-recursion-intro.js
@@ -0,0 +1,9 @@
+// ------------------------------------- Recursividad ----------------------------------------------
+/* 
+    La recursividad es un proceso en el que una función se llama a sí misma a sí misma, pero con dife-
+    rente input cada vez y hasta que se llega a un base case.
+    Por lo tanto, dos elementos clave para la recursividad son:
+        * Base case (condición donde termina).
+        * Input diferente.
+    
+*/
