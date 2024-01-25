abb1f0b22a4bb9f3af300347ae3e95196c291863
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-24 22:10:18 -0600

    swp - practice challenge 03 (course solution)

--- 03-problem-solving-patterns/03-sliding-window-pattern/swp-04-challenge03.js
+++ 03-problem-solving-patterns/03-sliding-window-pattern/swp-04-challenge03.js
@@ -41,3 +41,22 @@ function findLongestSubstring(str){
 }
 findLongestSubstring('thisisawesome'); // 6
 findLongestSubstring('rithmschool'); // 7
+
+// ---------------- Solución del curso ----------------
+function findLongestSubstring(str) {
+    let longest = 0;
+    let seen = {};
+    let start = 0;
+   
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if (seen[char]) {
+            start = Math.max(start, seen[char]);
+        }
+        // index - beginning of substring + 1 (to include current in count)
+        longest = Math.max(longest, i - start + 1);
+        // store the index of the next char so as to not double count
+        seen[char] = i + 1;
+    }
+    return longest;
+}
