260d05fc4657d16bfade617a2c3bb9f031f75d91
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-25 19:57:45 -0600

    hmr - example

--- 04-recursion/03-helper-method-recursion.js
+++ 04-recursion/03-helper-method-recursion.js
@@ -7,3 +7,26 @@
     hay otra, la función recursiva.
     Esto se hace comúnmente cuando necesitamos compilar algo como un array o una lista de datos.
 */
+
+// Ejemplo:
+function collectOddValues(arr){
+    
+    let result = []
+
+    function helper(helperInput){
+        if(helperInput.length === 0) {
+            return;
+        }
+        
+        if(helperInput[0] % 2 !== 0){
+            result.push(helperInput[0])
+        }
+        
+        helper(helperInput.slice(1))
+    }
+    
+    helper(arr)
+
+    return result;
+
+}
