d4d8f91ee452da6b14fcfb1c077d7a48eba39525
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-25 19:56:47 -0600

    helper method recursion explanation

---
+++ 04-recursion/03-helper-method-recursion.js
@@ -0,0 +1,9 @@
+// ------------------------------ Helper Method Recursion ----------------------------------------
+/* 
+    El Helper Method Recursion es un patrón de diseño comúnmente utilizado con recursión. Hasta ahora, 
+    todas las funciones recursivas que hemos visto son funciones independientes e individuales, es 
+    decir, una función que al ejecutarse, se llama a sí misma.
+    Con este nuevo método es un poco diferente. Se tienen dos funciones: la normal y dentro de esta 
+    hay otra, la función recursiva.
+    Esto se hace comúnmente cuando necesitamos compilar algo como un array o una lista de datos.
+*/
