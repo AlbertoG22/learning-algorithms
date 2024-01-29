ef451e889c2f649dbc922cb514c257767977f533
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-29 13:13:27 -0600

    recursion - practice challenge 04 (problem description)

---
+++ 04-recursion/07-challenge04.js
@@ -0,0 +1,10 @@
+// ------------------------------ Recursive Range Challenge -----------------------------------------
+/* 
+    Problema:   
+    Escriba una función llamada 'recursiveRange' la cual acepte un número y sume todos los números 
+    desde 0 hasta el número pasado a la función.
+
+    Output:
+    recursiveRange(6); // 21
+    recursiveRange(10); // 55 
+*/
