dfe40748fea9acca667213b2023b3c7aa9466f70
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-29 12:10:26 -0600

    recursion - practice challenge 01 (problem description)

---
+++ 04-recursion/04-challenge01.js
@@ -0,0 +1,13 @@
+// ----------------------------------- Power Challenge ----------------------------------------------
+/* 
+    Problema:   
+    Escribe una función llamada 'power' que acepte una base y un exponente. La función debe devolver 
+    la potencia de la base elevado al exponente. Esta función debería imitar la funcionalidad de 
+    Math.pow(); no se preocupe por las bases y exponentes negativos.
+    - Solución debe ser O(n).
+
+    Output:
+    power(2,0) // 1
+    power(2,2) // 4
+    power(2,4) // 16
+*/
