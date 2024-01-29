0b6c1d06dc56b6aad860d2d42e037871bd1eb3c2
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-29 12:24:41 -0600

    recursion - practice challenge 03 (problem description)

---
+++ 04-recursion/06-challenge03.js
@@ -0,0 +1,6 @@
+// ------------------------------ Product of Array Challenge -----------------------------------------
+/* 
+    Problema:   
+    Escriba una función llamada 'productOfArray' que tome una serie de números y devuelva el producto 
+    de todos ellos.
+*/
