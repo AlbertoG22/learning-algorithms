95b973fcef6c3b0afdf460133f5bacf9e0307bea
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-29 13:52:20 -0600

    recursion - practice challenge 05 (course solution)

--- 04-recursion/08-challenge05.js
+++ 04-recursion/08-challenge05.js
@@ -32,3 +32,10 @@ function fib(n) {
     return fibArr[n - 1];
 }
 fib(4); // 3
+
+
+// ---------------- Solución del curso ----------------
+function fib(n){
+    if (n <= 2) return 1;
+    return fib(n-1) + fib(n-2);
+}
