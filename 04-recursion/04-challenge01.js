8d6b7f0b1be19323fd9ecec2e26ff466a3fe93f6
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-29 12:15:36 -0600

    recursion - practice challenge 01 (my solution)

--- 04-recursion/04-challenge01.js
+++ 04-recursion/04-challenge01.js
@@ -11,3 +11,10 @@
     power(2,2) // 4
     power(2,4) // 16
 */
+
+// ---------------- Mi solución ----------------
+function power(base, exp) {
+    if(exp === 0) return 1;
+    return base * power(base, exp - 1);
+}
+power(2,4); // 16
