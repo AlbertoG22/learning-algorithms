f413abdf26cbaeb6977eac88b610cb0202437d6e
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-29 18:52:07 -0600

    recursion hard - challenge 01 (problem description)

---
+++ 04-recursion/02-harder-problems/01-reverse-challenge01.js
@@ -0,0 +1,10 @@
+// ----------------------------------- Reverse Challenge -------------------------------------------
+/* 
+    Problema:   
+    Escriba una función recursiva llamada 'reverse' que acepte un string y devuelva un nuevo string 
+    a la inversa.
+
+    Output:
+    reverse('awesome'); // 'emosewa'
+    reverse('rithmschool'); // 'loohcsmhtir'
+*/
