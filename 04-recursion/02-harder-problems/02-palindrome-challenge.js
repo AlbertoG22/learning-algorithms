841ec2e0b93a17f8627c380f4987ed6c185fbf2b
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-30 17:22:21 -0600

    recursion hard - challenge 02 (problem description)

---
+++ 04-recursion/02-harder-problems/02-palindrome-challenge.js
@@ -0,0 +1,13 @@
+// ----------------------------------- IsPalindrome Challenge ----------------------------------------
+/* 
+    Problema:   
+    Escriba una función recursiva llamada 'isPalindrome' que devuelva true si el string que se le 
+    pasa es un palíndromo (se lee igual hacia adelante y hacia atrás). De lo contrario devuelva false.
+
+    Output:
+    isPalindrome('awesome'); // false
+    isPalindrome('foobar'); // false
+    isPalindrome('tacocat'); // true
+    isPalindrome('amanaplanacanalpanama'); // true
+    isPalindrome('amanaplanacanalpandemonium'); // false
+*/
