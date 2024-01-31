1ef5f8d65f9e0ee1d840bfca45a7fcf1b564b437
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-30 19:14:46 -0600

    recursion hard - challenge 03 (my solution)

--- 04-recursion/02-harder-problems/03-someRecursive-challenge.js
+++ 04-recursion/02-harder-problems/03-someRecursive-challenge.js
@@ -1,7 +1,7 @@
 // ----------------------------------- someRecursive Challenge ----------------------------------------
 /* 
     Problema:   
-    Escriba una función recursiva llamada 'someRecursive' que acepte un aray y un callback. La función
+    Escriba una función recursiva llamada 'someRecursive' que acepte un array y un callback. La función
     deberá retornar true si solo un valor del array retorna true cuando se pase al callback. De lo
     contrario retorne false.
 
@@ -14,3 +14,15 @@
     someRecursive([4,6,8], isOdd); // false
     someRecursive([4,6,8], val => val > 10); // false
 */
+
+// ---------------- Mi solución ----------------
+const isOdd = val => val % 2 !== 0;
+
+function someRecursive(arr, fn) {
+    if(arr.length === 0) return false;
+    if(fn(arr[0])) return true;
+
+    return someRecursive(arr.slice(1), fn);
+}
+someRecursive([1,2,3,4], isOdd); // true
+someRecursive([4,6,8], val => val > 10); // false
