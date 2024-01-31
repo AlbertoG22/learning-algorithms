12e23d1004be6bd9cdacfab16af2b3ce10088fdc
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-30 19:17:43 -0600

    recursion hard - challenge 04 (problem description)

---
+++ 04-recursion/02-harder-problems/04-flatten-challenge.js
@@ -0,0 +1,12 @@
+// ----------------------------------- Flatten Challenge ----------------------------------------
+/* 
+    Problema:   
+    Escriba una función recursiva llamada 'flatten' que acepte un array de arrays y retorne un nuevo
+    array con todos los valores aplanados.
+
+    Output:
+    flatten([1, 2, 3, [4, 5] ]); // [1, 2, 3, 4, 5]
+    flatten([1, [2, [3, 4], [[5]]]]); // [1, 2, 3, 4, 5]
+    flatten([[1],[2],[3]]); // [1,2,3]
+    flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]); // [1,2,3
+*/
