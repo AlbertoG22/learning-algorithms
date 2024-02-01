fa63bd02637180402fea4e9641faaaee4cbf4622
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-01-31 18:33:53 -0600

    recursion hard - challenge 01 (course solution) explanation

--- 04-recursion/02-harder-problems/01-reverse-challenge01.js
+++ 04-recursion/02-harder-problems/01-reverse-challenge01.js
@@ -28,3 +28,38 @@ function reverse(str){
 	if(str.length <= 1) return str;
 	return reverse(str.slice(1)) + str[0];
 }
+reverse('awesome'); // 'emosewa'
+
+/* 
+    Explanation:
+    - Lo que se va haciendo es llamarse a sí mismo cada vez pero sin el primer elemento del string.
+    - Al momento que llega a la última letra y se vuelve a llamar solo con ella, en el ejemplo de 
+    arriba ↑ vemos que es la letra 'e', entra en la condición y se retorna esta letra, haciendo la
+    suma a la inversa, es decir 'e' + 'm' = 'em'.
+    - Y a su vez, este resultado se le va sumando la anterior, sucesivamente.
+    - Esto es el motivo de siempre después de llamar a la función, sumarle la letra que le quitamos.
+*/
+
+/* 
+    ------------------ 'awesome' ------------------
+    1°: return  fn('wesome') + 'a'
+    2°: return  fn('esome') + 'w'
+    3°: return  fn('some') + 'e'
+    4°: return  fn('ome') + 's'
+    5°: return  fn('me') + 'o'
+    6°: return  fn('e') + 'm'
+        return     'e'
+
+    Al final, la última llamada a la función retorna la misma letra con la que se mandó llamar, y ahora
+    se van sumando con la penúltima letra y así sucesivamente...
+
+    6°: return  'e' + 'm' = 'em'
+    5°: return  'em' + 'o' = 'emo'
+    4°: return  'emo' + 's' = 'emos'
+    3°: return  'emos' + 'e' = 'emose'
+    2°: return  'emose' + 'w' = 'emosew'
+    1°: return  'emosew' + 'a' = 'emosewa'
+        return  'emosewa'
+    
+    ...formando la secuencia del string a la inversa.
+*/
