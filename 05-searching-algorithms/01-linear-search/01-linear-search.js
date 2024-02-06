8e726f8e4cd4a8525ce18eaf37d05313559ac8ed
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-02-06 13:42:40 -0600

    linear search 01 (my solution)

--- 05-searching-algorithms/01-linear-search/01-linear-search.js
+++ 05-searching-algorithms/01-linear-search/01-linear-search.js
@@ -13,7 +13,25 @@
 
 /* 
     Problema:   
-    Escriba una función que acepte un array y un valor. La funcipon deberá recorrer el array buscando
-    el segundo elemento que se le pasó como parámetro. Si lo encuentra, debe retornar el índice donde
-    se encuentra, si no, retornará -1.
+    Escriba una función llamada 'linearSearch' que acepte un array y un valor, y retorne el índice en 
+    el que existe el valor. Si el valor no existe en el array, retorne -1.
+
+    ¡No utilice indexOf para resolver el problema!
+
+    Output:
+    linearSearch([10, 15, 20, 25, 30], 15); // 1
+    linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 4); // 5
+    linearSearch([100], 100); // 0
+    linearSearch([1,2,3,4,5], 6); // -1
+    linearSearch([9, 8, 7, 6, 5, 4, 3, 2, 1, 0], 10); // -1
+    linearSearch([100], 200); // -1
 */
+
+// ---------------- Mi solución ----------------
+function linearSearch(arr, num) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === num) return i;
+    }
+    return -1;
+}
+linearSearch([10, 15, 20, 25, 30], 15); // 1
