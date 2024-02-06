82e60b5b4ccbfef2869b325d47d1447e9d4aa703
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-02-06 13:33:32 -0600

    linear search 01 (problem description)

---
+++ 05-searching-algorithms/01-linear-search/01-linear-search.js
@@ -0,0 +1,7 @@
+// ------------------------------- Linear Search Challenge -------------------------------------------
+/* 
+    Problema:   
+    Escriba una función que acepte un array y un valor. La funcipon deberá recorrer el array buscando
+    el segundo elemento que se le pasó como parámetro. Si lo encuentra, debe retornar el índice donde
+    se encuentra, si no, retornará -1.
+*/
