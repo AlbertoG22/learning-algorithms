6cf326b4ea943ea57fea905730d7b890374bfe45
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-02-06 13:36:19 -0600

    linear serach intro

--- 05-searching-algorithms/01-linear-search/01-linear-search.js
+++ 05-searching-algorithms/01-linear-search/01-linear-search.js
@@ -1,5 +1,17 @@
 // ------------------------------- Linear Search Challenge -------------------------------------------
 /* 
+    Linear Search es un mecanismo en el que se hace una búsqueda elemento por elemento, es decir, 
+    suponiendo que tengamos un array con varios elementos y queremos encontrar uno en específico, lo 
+    que hace es recorrer uno a uno el array y verificar si es el que buscamos, si no, sigue con el 
+    siguiente hasta encontrarlo.
+    Behind the scenes, JavaScript aplica esto en algunos métodos de arrays:
+        * indexOf
+        * includes
+        * find
+        * findIndex
+*/
+
+/* 
     Problema:   
     Escriba una función que acepte un array y un valor. La funcipon deberá recorrer el array buscando
     el segundo elemento que se le pasó como parámetro. Si lo encuentra, debe retornar el índice donde
