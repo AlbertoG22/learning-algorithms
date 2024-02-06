bdf3f8abb2168b2bb2e609997241fd3dd418d1ca
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-02-06 14:02:24 -0600

    linear search 01 (course solution)

--- 05-searching-algorithms/01-linear-search/01-linear-search.js
+++ 05-searching-algorithms/01-linear-search/01-linear-search.js
@@ -35,3 +35,13 @@ function linearSearch(arr, num) {
     return -1;
 }
 linearSearch([10, 15, 20, 25, 30], 15); // 1
+
+
+// ---------------- Solución del curso ----------------
+function linearSearch(arr, val) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === val) return i;
+    }
+    return -1;
+}
+// la misma solución
