18f748558779961b1694006d2ef4163afe78248f
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2024-02-14 16:35:31 -0600

    binary search intro

---
+++ 05-searching-algorithms/02-binary-search/01-binary-search-intro.js
@@ -0,0 +1,9 @@
+// ------------------------------- Binary Search Challenge -------------------------------------------
+/* 
+    La idea de este patrón es "Divide and Conquer", tomar la mitad del array y preguntar si lo que 
+    buscamos está antes o después de ese elemento central, dependiendo de esto, se toma o descarta 
+    ciertos elementos del array. Este proceso se repite hasta que se encuentra el elemento deseado 
+    (o no), lo cual disminuye considerablemente el número de ejecuciones que con la búsqueda lineal.
+    
+    Este mecanismo es mucho más rápido que el linear.
+*/
